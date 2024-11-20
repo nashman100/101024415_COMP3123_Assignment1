b711e3fbaf4e666168babce606c818035fa99349
nashman100 <nashgill93@gmail.com>
2024-11-20 05:30:25 -0500

    changed baseURL

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8081/api/v1',
+  baseURL: 'https://101024415-comp-3123-assignment1.vercel.app/api/v1',
 });
 
 API.interceptors.request.use((req) => {
@@ -15,3 +15,4 @@ API.interceptors.request.use((req) => {
 export default API;
 
 //https://101024415-comp-3123-assignment1.vercel.app/api/v1
+//http://localhost:8081/api/v1
